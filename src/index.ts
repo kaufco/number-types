b51f00881911e7958ca878b1dea3092bac60ab0d
Marco Kaufmann <Marco.Kaufmann@dmtm.com>
2021-06-24 02:04:30 +0200

    fixed broken links in API doc

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,11 @@ export type int = number;
 export type long = bigint;
 
 /**
- * A `number` that is used as a (_IEEE-754 Single_)[https://en.wikipedia.org/wiki/Single-precision_floating-point_format].
+ * A `number` that is used as a [_IEEE-754 Single_](https://en.wikipedia.org/wiki/Single-precision_floating-point_format).
  */
 export type float = number;
 
 /**
- * A `number` that is used as a (_IEEE-754 Double_)[https://en.wikipedia.org/wiki/Double-precision_floating-point_format].
+ * A `number` that is used as a [_IEEE-754 Double_](https://en.wikipedia.org/wiki/Double-precision_floating-point_format).
  */
 export type double = number;
